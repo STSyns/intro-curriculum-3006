cf04920c066466ae039699156aecc6a0330e0ec0
STSyns <sacculia@gmail.com>
2018-10-20 00:17:58 +0000

    Lot実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,10 @@
 'use strict';
+const lots = ['super lucky', 'lucky', 'small lucky', 'bad', 'super bad'];
+const lot = lots[Math.floor(Math.random() * lots.length)];
+
 module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
+	robot.hear(/lot>/i, (msg) => {
 		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+		msg.send('Hello, ' + username + '. ->' + lot);
 	});
 };
